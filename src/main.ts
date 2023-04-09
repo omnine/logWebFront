9c13a584927dfca998de352b0cfed6d753f97ed6
Mingfa Ma <mingfa.ma@deepnetsecurity.com>
2023-04-09 10:09:02 +0100

    initial

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import { LitElement, html} from 'lit';
+
+import { customElement, property } from 'lit/decorators.js';
+
+
+@customElement('comp-main')
+export class CompMain extends LitElement {
+
+    @property({ type: String }) message: string = 'IIS Agent RealTime Log';
+
+    @property()
+    message1: string = 'Hello again.';
+    message2: string = 'Hello again.';
+    recordingStatus: boolean = false;
+
+    recording(event: Event) {
+        if(this.recordingStatus) {
+            this.message1 = "Recording....";
+        }
+        else {
+            this.message1 = "You will get the records soon";
+        }
+        this.recordingStatus = !this.recordingStatus;
+    }
+
+    render() {
+        return html`
+        <div>
+            <h1>${this.message}</h1>
+            <p>Normally IIS log is very big, it is hard to go through it. We just need to see the recent records, 
+                especially when the problem can be reproduced.</p>
+            <p>Please press the record button to start recording, then reproduce your problem.</p>
+            <p>Once the problem is reproduced, press the stop button.</p>
+            <button @click=${this.recording}>Record</button>
+            <button @click=${this.recording}>Stop</button>
+            <h2>${this.message1}</h2>
+            <ul>
+                <li>TypeScript</li>
+                <li>Rollup.js</li>
+                <li>es-dev-server</li>
+            </ul>
+        </div>
+        `;
+    }
+}
